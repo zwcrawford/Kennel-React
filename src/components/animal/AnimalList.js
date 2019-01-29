ab90b5b0fd0aa1f60779834b85161434a1e5f698
Zac Crawford <zwcrawford@gmail.com>
2019-01-29 08:43:30 -0600

    got the project back to it's original state after doing the challenge

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,34 +1,18 @@
-import React, { Component } from 'react';
-
-class AnimalList extends Component {
-    render() {
-
-      this.props.animals.map(animal => {
-        animal.owners = "";
-        animal.owner.map(name => {
-            let i = name - 1; // Counter
-            if (animal.owners === "") {
-                animal.owners = this.props.owners[i].name
-            } else {
-                animal.owners += `, ${this.props.owners[i].name} `
-            }
-            return animal.owners
-        })
-      })
-      return (
-          <article className="animals">
-          {
-              this.props.animals.map(animal =>
-                <div key={ animal.id }>
-                  {"Name:"} {animal.name}<br />
-                  {"Owner:"} {animal.owners}
-                </div>
-
-              )
-          }
-          </article>
-      )
+import React, { Component } from 'react'
+    class AnimalList extends Component {
+        render() {
+            return (
+                <section className="animals">
+                {
+                this.props.animals.map(animal =>
+                    <article>
+                        {animal.name}
+                    </article>
+                )
+                }
+                </section>
+            )
+        }
     }
-}
 
 export default AnimalList
